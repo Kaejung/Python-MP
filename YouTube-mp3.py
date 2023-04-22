62a7432111a85f5ebdbf677951e383850d41d609
Kaejung <122705981+Kaejung@users.noreply.github.com>
2023-04-22 17:25:47 +0200

    Add files via upload

--- YouTube-mp3.py
+++ YouTube-mp3.py
@@ -1,23 +1,23 @@
+import os
 from pytube import YouTube
 from moviepy.editor import *
-import os
 
-def convert_to_mp3(link):
-    # Chemin du dossier "Music" de Windows
-    music_folder = os.path.join(os.path.expanduser("~"), "Music")
-
-    # Télécharge la vidéo
-    video = YouTube(link).streams.first().download()
-
-    # Convertissement mp4 -> mp3 avec bitrate de 320 kbps
-    mp4_file = VideoFileClip(video)
-    mp3_file = mp4_file.audio.write_audiofile(os.path.join(music_folder, os.path.splitext(os.path.basename(video))[0] + ".mp3"), bitrate="320k", codec="libmp3lame")
-
-    # Supprime mp4
-    mp4_file.close()
-    os.remove(video)
-    print("Conversion complete!")
-
+#Coller le lien YouTube
 if __name__ == '__main__':
     link = input("Enter YouTube video link: ")
-    convert_to_mp3(link)
+
+#Chemin du dossier "Music" de Windows
+music_folder = os.path.join(os.path.expanduser("~"), "Music")
+
+#Télécharger la vidéo
+yt = YouTube(link)
+stream = yt.streams.get_highest_resolution()
+video_path = stream.download(output_path=music_folder)
+
+#Convertir la vidéo en mp3 avec une qualité audio de 320 kbps
+mp4_file = VideoFileClip(video_path)
+mp3_file = mp4_file.audio.write_audiofile(os.path.join(music_folder, os.path.splitext(os.path.basename(video_path))[0] + ".mp3"), bitrate="320k", codec="libmp3lame")
+
+#Supprimer le fichier vidéo
+mp4_file.close()
+os.remove(video_path)
